2b2d3e3585f21960138763dcaa198fc7bb7b624b
Nebula <me@itsnebula.net>
2023-04-16 18:52:28 -0400

    feat: meowcrypt decrypt in Apps menu

--- bridget/__main__.py
+++ bridget/__main__.py
@@ -3,13 +3,16 @@ import logging
 import discord
 import mongoengine
 import traceback
+import base64
+import hashlib
 
 from os import getenv
 mongoengine.connect('bridget', host=getenv("DB_HOST"), port=int(getenv("DB_PORT")))
 from discord.ext import commands
 from discord import app_commands
+from Crypto.Cipher import AES
 
-from utils import send_error
+from utils import send_error, send_success
 from utils.config import cfg
 from utils.startup_checks import checks
 from cogs import Logging, Mod, NativeActionsListeners, Say, Snipe, Sync, Tags, TagsGroup, Unshorten
@@ -25,6 +28,34 @@ bot = commands.Bot(
 )
 bot.remove_command("help")
 
+# Apps
+@bot.tree.context_menu(guild=discord.Object(id=cfg.guild_id), name="Meowcrypt Decrypt")
+async def meowcrypt_decrypt(interaction: discord.Interaction, message: discord.Message) -> None:
+    if "nya>.<" not in message.content:
+        await send_error(interaction, "The selected message is not encrypted by Meowcrypt.")
+        
+    spl = message.content.split(">.<")
+    one = base64.b64decode(spl[1])
+    two = base64.b64decode(spl[2])
+    three = base64.b64decode(spl[3])
+
+    pass_str = "8f5SCpAbDyCdtPTNBwQpYPJVussZFXVaVWP587ZNgZr3uxKGzRLf4naudDBxmdw5"
+    pass_bytes = pass_str.encode("utf-8")
+
+    key = hashlib.pbkdf2_hmac('sha512', pass_bytes, one, 50000, dklen=32)
+
+    cipher = AES.new(key, AES.MODE_GCM, nonce=two)
+    plaintext = cipher.decrypt_and_verify(three[:-16], three[-16:]).decode('utf-8')
+    
+    embed = discord.Embed(
+        title="Decrypted text",
+        description=f"```{plaintext}```",
+        color=discord.Color.green()
+    )
+    embed.set_author(name=message.author, icon_url=message.author.avatar.url)
+    await send_success(interaction, embed=embed, ephemeral=True)
+
+# Cogs
 asyncio.run(bot.add_cog(Logging(bot)))
 asyncio.run(bot.add_cog(Mod(bot)))
 asyncio.run(bot.add_cog(NativeActionsListeners(bot)))
@@ -35,6 +66,7 @@ asyncio.run(bot.add_cog(Tags(bot)))
 asyncio.run(bot.add_cog(TagsGroup(bot)))
 asyncio.run(bot.add_cog(Unshorten(bot)))
 
+# Error handler
 @bot.tree.error
 async def app_command_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
     if isinstance(error, app_commands.CommandInvokeError):
