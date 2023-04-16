8692d40e17d4eacea547b987d70cc28f4acb9cc0
June Park <rjp2030@outlook.com>
2023-04-16 09:27:28 +0900

    make kicks finally work

--- bridget/cogs/native_actions_listeners.py
+++ bridget/cogs/native_actions_listeners.py
@@ -11,9 +11,10 @@ class NativeActionsListeners(Cog):
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
         guild = member.guild
-        audit_logs = guild.audit_logs(limit=1, action=discord.AuditLogAction.kick)
-        if audit_logs:
-            add_kick_case(member, audit_logs[0].user, audit_logs[0].reason)
+        audit_logs = [audit async for audit in guild.audit_logs(limit=1, action=discord.AuditLogAction.kick, after=member.joined_at)]
+        if audit_logs and audit_logs[0].target == member:
+            channel = member.guild.get_channel(guild_service.get_guild().channel_public)
+            await channel.send(embed=add_kick_case(member, audit_logs[0].user, "No reason." if audit_logs[0].reason is None else audit_logs[0].reason, guild_service.get_guild()))
     
     @commands.Cog.listener()
     async def on_automod_action(self, ctx: discord.AutoModAction):
