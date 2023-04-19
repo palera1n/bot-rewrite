1db373e7ccc45ea5f559f38c55d75a1d45f89740
Jan Garcia <jancraft888@gmail.com>
2023-04-19 16:14:11 +0200

    make /postembed and /poll check for send permission

--- bridget/cogs/helper.py
+++ bridget/cogs/helper.py
@@ -58,6 +58,12 @@ class Helper(Cog):
         if channel is None:
             channel = ctx.channel
 
+        # check if the user has permission to send to that channel
+        perms = channel.permissions_for(ctx.user)
+        if not perms.send_messages:
+            await send_error(ctx, "You can't send messages in that channel!", delete_after=1)
+            return
+
         # create the embed, add the image and color if specified
         embed = discord.Embed(title=title)
         if image is not None:
@@ -95,6 +101,12 @@ class Helper(Cog):
         # the interaction was ran in
         if channel is None:
             channel = ctx.channel
+
+        # check if the user has permission to send to that channel
+        perms = channel.permissions_for(ctx.user)
+        if not perms.send_messages:
+            await send_error(ctx, "You can't send messages in that channel!", delete_after=1)
+            return
 
         # create the embed, add the image and color if specified
         embed = discord.Embed(
